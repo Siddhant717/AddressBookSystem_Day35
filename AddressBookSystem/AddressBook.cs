f82f9db19b3787ae45d3f748d48cb84aa8b85bb7
Siddhant <santoshshrivastava39@gmail.com>
2022-08-03 15:34:33 +0530

    Insert new contacts

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class AddressBook
+    {
+        static List<Contact> addressbook = new List<Contact>();
+        Contact contact = new Contact();
+        public void AddNewContacts()
+        {
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Enter your First name-");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter your Last name-");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter your Address-");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter your City-");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter your State-");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter your Zipcode-");
+            contact.Zipcode = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your PhoneNumber-");
+            contact.PhoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your Email-");
+            contact.Email = Console.ReadLine();
+
+            addressbook.Add(contact);
+
+
+        }
+
+        public void Display()
+        {
+            foreach (var data in addressbook)
+            {
+                Console.WriteLine("The Details that you have added are -");
+                Console.WriteLine("First Name: " + data.FirstName);
+                Console.WriteLine("Last Name: " + data.LastName);
+                Console.WriteLine("Enter Address: " + data.Address);
+                Console.WriteLine("City: " + data.City);
+                Console.WriteLine("State: " + data.State);
+                Console.WriteLine("Zip: " + data.Zipcode);
+                Console.WriteLine("Phone Number: " + data.PhoneNumber);
+                Console.WriteLine("Email: " + data.Email);
+            }
+            Console.WriteLine("Details added successfully");
+
+        }
+    }
+}
+        
+
+
+        
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook contactdetails = new();
+            while (true)
+            {
+                Console.WriteLine("Enter the option : \n1)Adding Contact \n2)Display contact");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        contactdetails.AddNewContacts();
+                        break;
+                    case 2:
+                        contactdetails.Display();
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Please Choose From Above Given Options");
+                        break;
+                }
+            }
+        }
+    }
+}
